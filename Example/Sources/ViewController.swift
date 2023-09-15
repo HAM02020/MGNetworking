a8394143b39a83cfd6d46c64ae031789356fecef
莫凡圣 <fansheng.mo@zkteco.com>
2023-09-15 15:14:51 +0800

    搭建项目骨架

---
+++ Example/Sources/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Example
+//
+//  Created by sheng on 2023/9/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
